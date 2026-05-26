1ed3e91a3150fe5a36422c75e48436d73281b194
Fabianne Arezes <fabiannearezes0904@gmail.com>
2026-05-25 21:33:16 -0300

    resolvendo bugs e retirando a aba de favoritos e notificações

--- backend/settings.py
+++ backend/settings.py
@@ -177,6 +177,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # 3. Configurações de CORS (Permite comunicação com o React)
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
